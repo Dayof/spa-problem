eaf6a7b47eecf895b1e7df3eb7a7970b7bf4e1eb
Day <dayannefernandesc@gmail.com>
2017-05-25 11:56:49 -0300

    partial pseudocode of spa problem

--- test_alg.py
+++ test_alg.py
@@ -1,18 +1,34 @@
-def checkIfSchoolHasVacancy(graph, t, s, v):
-    for i in s:
-        if graph[t][1] == graph[i][1] and v[i]:
-            return s
-    return -1
-
+def worstTeacher(s, t):
+    if graph[t][1] != graph[s][1]:
+        return True
+    elif s not in graph[t][2]:
+        return True
+    else:
+        return False
 
 def spa_teacher(graph):
     tf, sf, vf = [0, 1, 2], [3, 4, 5], {3:2, 4:2, 5:2}
     ft = tf[0]
+    final_match = {}
     while ft in tf and graph[ft][2]:
-        vos = checkIfSchoolHasVacancy(graph, ft, graph[ft][2], vf)
-        if vos > -1:
-            vf[vos] -= 1
-        else:
+        fst = graph[ft][2][0]
+        final_match[fst] = [ft]
+        vf[fst] -= 1
+        # school is over subscribed
+        if vf[fst] < 0:
+            removed = False
+            for j in final_match[fst]:
+                result = worstTeacher(fst, j)
+                if result:
+                    final_match[fst].remove(j)
+                    removed = True
+                    vf[fst] += 1
+                    break
+
+            if not removed:
+                final_match[fst].remove(j)
+        # school is full
+        if vf[fst] == 0:
             pass
 
 graph = {}
